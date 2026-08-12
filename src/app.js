f68c6ad46e080f575f389bfba3a29a3634c6c075
NgnPt <nguyendpham101@gmail.com>
2026-08-12 15:27:18 +0700

    feat: setup express api

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+module.exports = app;
