e758ce7773fb4f0f31dbd1445c38f33bf71a9074
NgnPt <nguyendpham101@gmail.com>
2026-08-12 15:29:08 +0700

    feat: add swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const todoRoutes = require('./routes/todo.routes');
 
 const app = express();
+
+const swaggerDocument = YAML.load(path.join(__dirname, '..', 'swagger', 'openapi.yaml'));
 
 app.use(express.json());
 
@@ -9,6 +14,7 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/todos', todoRoutes);
 
 app.use((req, res) => {
--- src/server.js
+++ src/server.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Todo API listening on http://localhost:${PORT}`);
+  console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
 });
